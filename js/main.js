336f64df2993aa35df34a973a3676113e92aa48b
youngjinjung <jyj1641@gmail.com>
2024-11-20 19:13:38 +0900

    스크롤 메뉴 Active 추가: IntersctionObserver

--- js/main.js
+++ js/main.js
@@ -14,3 +14,28 @@ function init() {
 }
 
 init();
+
+const options = {
+  threshold: 1,
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const sectionId = entry.target.id;
+      console.log(sectionId);
+      document
+        .querySelector(`.nav__link[href*=${sectionId}]`)
+        .classList.add('active-link');
+
+      document
+        .querySelectorAll(`.nav__link:not([href*=${sectionId}])`)
+        ?.forEach((el) => el.classList.remove('active-link'));
+    }
+  });
+}, options);
+
+const $sectionList = document.querySelectorAll('.section');
+$sectionList.forEach((el) => {
+  observer.observe(el);
+});
